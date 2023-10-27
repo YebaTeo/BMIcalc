d72f01d569dad4553ad31a6ffaba4bf5189a2433
YebaTeo <yebateo24@gmail.com>
2023-10-27 23:26:47 +0700

    Third Commit

--- calculateBMI.js
+++ calculateBMI.js
@@ -2,22 +2,22 @@ function calculateBMI() {
     const height = parseFloat(document.getElementById("height").value);
     const weight = parseFloat(document.getElementById("weight").value);
     const resultElement = document.getElementById("bmiResult"); // Reference to the result element
-    
+    const name = document.getElementById("name").value; // Get the name input
     if (height && weight) {
         const bmi = weight / Math.pow(height / 100, 2);
         resultElement.textContent = bmi.toFixed(2);
-
+        document.getElementById("displayName").textContent = name; // Display the name
         // Determine and display the BMI category
         if (bmi < 18.5) {
-            resultElement.innerHTML += "<br>You are Underweight!";
+            resultElement.innerHTML += '<br>You are <span class = "bg-info">Underweight!</span>';
         } else if (bmi >= 18.5 && bmi <= 24.9) {
-            resultElement.innerHTML += "<br>You are Normal!";
+            resultElement.innerHTML += '<br>You are <span class = "bg-info">Normal!</span>';
         } else if (bmi >= 25 && bmi <= 29.9) {
-            resultElement.innerHTML += "<br>You are Overweight!";
+            resultElement.innerHTML += '<br>You are <span class = "bg-info">Overweight!</span>';
         } else if (bmi >= 30 && bmi <= 34.9) {
-            resultElement.innerHTML += "<br>You are Obese!";
+            resultElement.innerHTML += '<br>You are <span class = "bg-info">Obese!</span>';
         } else if (bmi > 35) {
-            resultElement.innerHTML += "<br>You are Extremely Obese!";
+            resultElement.innerHTML += '<br>You are <span class = "bg-info">Extremely Obese!</span>';
         }
     } else {
         alert("Please enter valid height and weight values.");
